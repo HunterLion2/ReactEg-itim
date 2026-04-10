19f5d4cf9ae97b43c8cac8e2989d0df94e4d5fce
Çınar Özgürgil <109600067+HunterLion2@users.noreply.github.com>
2026-04-10 20:31:26 +0300

    MovieAppProjectLast

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,16 +1,11 @@
-export default function Header() {
-  // Burada html yazdığımız return olmak zorundadır çünkü burası aslında bir js'dir bizim bunu burada döndürmemiz için bu gereklidir.
+export default function Header({ children }) {
   return (
     <div id="header">
       <nav
         className="navbar navbar-expand-lg bg-dark border-bottom border-body"
         data-bs-theme="dark"
       >
-        <div className="container">
-          <a href="#" className="navbar-brand">
-            Movie App
-          </a>
-        </div>
+        <div className="container">{children}</div>
       </nav>
     </div>
   );
--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -4,9 +4,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
-
-// Buradaki alan da main.jsx aslında ana merkez konumundadır ve aşşağıda toplama kampız olan App.jsx'i çağırırız.
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
